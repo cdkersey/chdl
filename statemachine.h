0a4175300397001e23a4d431250e0f2cd6ab457b
cdkersey <chad.d.kersey@gmail.com>
2013-03-13 19:08:51 -0400

    State machine builder.

---
+++ statemachine.h
@@ -0,0 +1,38 @@
+#ifndef __STATEMACHINE_H
+#define __STATEMACHINE_H
+
+#include "bvec-basic-op.h"
+#include "adder.h"
+#include "shifter.h"
+#include "enc.h"
+#include "mux.h"
+#include "tap.h"
+
+namespace chdl {
+  // A state machine with N states.
+  template <unsigned N> class Statemachine {
+   public:
+  Statemachine(): nextState(Enc(nextState1h)), state(Reg(nextState)) {
+      state1h = Zext<N>(Decoder(state));
+      TAP(nextState1h); TAP(nextState);
+    }
+
+    void edge(unsigned from, unsigned to, node take) {
+      edges[to][from] = take && state1h[from];
+    }
+
+    void generate() {
+      for (unsigned i = 0; i < N; ++i)
+        nextState1h[i] = OrN(edges[i]);
+    }
+
+    operator bvec<CLOG2(N)>() { return state; }
+
+   private:
+    vec<N, bvec<N>> edges;
+    bvec<N> nextState1h, state1h;
+    bvec<CLOG2(N)> nextState, state;
+  };
+};
+
+#endif
