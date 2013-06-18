50ac83b144fa8be8b5524aacc544ff3e2fbbb442
cdkerse <cdkerse@sandia.gov>
2013-06-18 15:58:05 -0600

    Added main file for running in verilator.

---
+++ examples/verilator_main.cpp
@@ -0,0 +1,42 @@
+#include "Vexample2.h"
+
+#include <verilated.h>
+#include <verilated_vcd_c.h>
+
+#include <iostream>
+
+#include <sys/time.h>
+
+using namespace std;
+
+unsigned long time_us() {
+  struct timeval tv;
+  gettimeofday(&tv, NULL);
+  return 1000000ul * tv.tv_sec + tv.tv_usec;
+}
+
+int main(int argc, char **argv) {
+  Verilated::commandArgs(argc, argv);
+  Verilated::traceEverOn(true);
+  Vexample2 *top = new Vexample2;
+  VerilatedVcdC trace_obj;
+
+
+  unsigned long main_time(0), start(time_us());
+  top->trace(&trace_obj, 1);
+  trace_obj.open("dump.vcd");
+  while (main_time < 2000) {
+    top->phi = !top->phi;
+    top->eval();
+    trace_obj.dump(main_time);
+    ++main_time;
+  }
+  trace_obj.close();
+
+  unsigned long duration(time_us() - start);
+
+  cerr << duration/1000.0 << "ms\n";
+
+  delete top;
+  return 0;
+}
