246fc538095340865f936527b19775fd999b3c40
cdkerse <cdkerse@sandia.gov>
2013-06-24 16:57:08 -0600

    Example 6 is more interesting and time is important.

--- examples/verilator_main.cpp
+++ examples/verilator_main.cpp
@@ -1,15 +1,28 @@
-#include "Vexample2.h"
+#include "Vexample6.h"
 
 #include <verilated.h>
 #include <verilated_vcd_c.h>
 
+#include <iostream>
+
+#include <sys/time.h>
+
+using namespace std;
+
+unsigned long time_us() {
+  struct timeval tv;
+  gettimeofday(&tv, NULL);
+  return 1000000ul * tv.tv_sec + tv.tv_usec;
+}
+
 int main(int argc, char **argv) {
   Verilated::commandArgs(argc, argv);
   Verilated::traceEverOn(true);
-  Vexample2 *top = new Vexample2;
+  Vexample6 *top = new Vexample6;
   VerilatedVcdC trace_obj;
 
-  unsigned long main_time(0);  
+
+  unsigned long main_time(0), start(time_us());
   top->trace(&trace_obj, 1);
   trace_obj.open("dump.vcd");
   while (main_time < 2000) {
@@ -18,8 +31,11 @@ int main(int argc, char **argv) {
     trace_obj.dump(main_time);
     ++main_time;
   }
-
   trace_obj.close();
+
+  unsigned long duration(time_us() - start);
+
+  cerr << duration/1000.0 << "ms\n";
 
   delete top;
   return 0;
