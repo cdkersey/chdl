2a12cd3b0ecbfaf595deb3b34911dcef9e8ceb9f
cdkersey <chad.d.kersey@gmail.com>
2014-01-28 18:25:08 -0500

    Finished updating enc.h.

--- enc.h
+++ enc.h
@@ -9,72 +9,56 @@
 #include "hierarchy.h"
 
 namespace chdl {
-  static bvec<1> PriEnc(node &valid, const bvec<2> &in) {
+  static bvec<1> PriEnc(node &valid, const bvec<2> &in, bool reverse=false) {
+    HIERARCHY_ENTER();
     valid = in[0] || in[1];
-    return in[1];
+    node ret;
+    if (reverse) ret = !in[0];
+    else         ret = in[1];
+    HIERARCHY_EXIT();
+
+    return ret;
   }
 
-  template <unsigned N> bvec<CLOG2(N)> PriEnc(node &valid, const bvec<N> &in) {
+  template <unsigned N>
+    bvec<CLOG2(N)> PriEnc(node &valid, const bvec<N> &in, bool reverse = false)
+  {
+    HIERARCHY_ENTER();
+
     const unsigned A(N/2), B(N - A);
 
     bvec<A> rin(in[range<0, A-1>()]);
     node rvalid;
-    bvec<CLOG2(A)> r(PriEnc(rvalid, rin));
+    bvec<CLOG2(A)> r(PriEnc(rvalid, rin, reverse));
 
     bvec<B> lin(in[range<A, N-1>()]);
     node lvalid;
-    bvec<CLOG2(A)> l(Zext<CLOG2(A)>(PriEnc(lvalid, lin)));
+    bvec<CLOG2(A)> l(Zext<CLOG2(A)>(PriEnc(lvalid, lin, reverse)));
 
     valid = lvalid || rvalid;
 
-    return Cat(lvalid, Mux(lvalid, r, l));
-  }
+    bvec<CLOG2(N)> ret;
 
-  template <unsigned N> bvec<CLOG2(N)> Log2(bvec<N> x) {
-    HIERARCHY_ENTER();
-
-    node valid;
-    return PriEnc(valid, x);
-
+    if (reverse)
+      ret = Cat(!rvalid, Mux(rvalid, l, r));
+    else
+      ret = Cat(lvalid, Mux(lvalid, r, l));
+ 
     HIERARCHY_EXIT();
+ 
+    return ret;
   }
 
-  #if 0
   // Integer logarithm unit (find index of highest set bit); priority encoder
   template <unsigned N> bvec<CLOG2(N)> Log2(bvec<N> x) {
     HIERARCHY_ENTER();
 
-    // First, produce the masks.
-    vec<N, bvec<N>> mask;
-    for (unsigned i = N/2; i > 0; i /= 2)
-      for (unsigned j = 0; j < N; ++j)
-        mask[i][j] = Lit(j%(i*2) >= i);
-
-    // Next, produce the masking chain.
-    bvec<CLOG2(N)> out;
-    vec<N+1, bvec<N>> val; val[N] = x;
-    for (unsigned i = N/2; i > 0; i /= 2) {
-      out[log2(i)] = OrN(mask[i] & val[i*2]);
-      val[i] = Mux(out[log2(i)], val[i*2], val[i*2] & mask[i]);
-    }
+    node valid;
+    bvec<CLOG2(N)> ret(PriEnc(valid, x));
 
     HIERARCHY_EXIT();
 
-    return out;
-  }
-  #endif
-
-  // Find index of lowest set bit; reverse of the Log2 priority encoder
-  template <unsigned N> bvec<CLOG2(N)> Lsb(bvec<N> x) {
-    bvec<CLOG2(N)> out;
-
-    HIERARCHY_ENTER();
-    bvec<N> xrev;
-    for (unsigned i = 0; i < N; ++i) xrev[N-i-1] = x[i];
-    out = ~Log2(xrev);
-    HIERARCHY_EXIT();
-
-    return out;
+    return ret;
   }
 
   // Cheap encoder, results valid only when exactly one input set
