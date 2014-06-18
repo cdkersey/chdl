97ebff84b19c11c69436c6887b220633e4370eb5
chad <chad.d.kersey@gmail.com>
2014-06-17 18:20:14 -0600

    Overloading was leading to unexpected errors. This should fix i.

--- sim.cpp
+++ sim.cpp
@@ -31,6 +31,25 @@ void chdl::print_time(ostream &out) {
   out << '#' << now << endl;  
 }
 
+void chdl::run(ostream &vcdout, function<bool()> end_condition,
+               unsigned threads)
+{
+  using namespace std;
+  using namespace chdl;
+
+  vector<unsigned> &ti(tick_intervals());
+
+  print_vcd_header(vcdout);
+  print_time(vcdout);
+  do {
+    print_taps(vcdout);
+    for (unsigned j = 0; j < ti.size(); ++j)
+      if (sim_time()%ti[j] == 0) advance(threads, j);
+    print_time(vcdout);
+  } while (!end_condition());
+}
+
+
 void chdl::run(ostream &vcdout, cycle_t time, unsigned threads) {
   run(vcdout, [time](){ return now == time; }, threads);
 }
--- sim.h
+++ sim.h
@@ -2,6 +2,7 @@
 #define __SIM_H
 
 #include <iostream>
+#include <functional>
 
 #include "tickable.h"
 #include "tap.h"
@@ -14,30 +15,9 @@ namespace chdl {
   void print_time(std::ostream&);
   cycle_t advance(unsigned threads=1, cdomain_handle_t cd = 0);
 
-  template <typename T>
-    void run(std::ostream &vcdout, T end_conditon, unsigned threads = 1);
-
   void run(std::ostream &vcdout, bool &stop, unsigned threads = 1);
   void run(std::ostream &vcdout, bool &stop, cycle_t max, unsigned threads = 1);
   void run(std::ostream &vcdout, cycle_t time, unsigned threads = 1);
+  void run(std::ostream &vcdout, std::function<bool()> end, unsigned threads=1);
 };
-
-template <typename T>
-  void chdl::run(std::ostream &vcdout, T end_condition, unsigned threads = 1)
-{
-  using namespace std;
-  using namespace chdl;
-
-  vector<unsigned> &ti(tick_intervals());
-
-  print_vcd_header(vcdout);
-  print_time(vcdout);
-  do {
-    print_taps(vcdout);
-    for (unsigned j = 0; j < ti.size(); ++j)
-      if (sim_time()%ti[j] == 0) advance(threads, j);
-    print_time(vcdout);
-  } while (!end_condition());
-}
-
 #endif
